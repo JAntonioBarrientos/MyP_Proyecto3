cd6581441c3f2f66fad86b74d264faa4c2ccab34
Gael <gaelchaparro@unam.mx.com>
2023-12-08 05:00:32 -0600

    Añadido de clases para encriptar y desincriptar

---
+++ proyecto3/src/main/java/mx/unam/ciencias/myd/proyecto3/DispersadorSHA256.java
@@ -0,0 +1,28 @@
+package mx.unam.ciencias.myd.proyecto3;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class DispersadorSHA256 {
+
+    /** Algoritmo de dispersion para la contrasña */
+    private static final String TIPO_DE_DISPERSION = "SHA-256";
+
+    /**
+     * Dispersa la cadena ingresada por el usuario con el algortimo SHA-256
+     * 
+     * @return la dispersion de la cadena en bytes
+     */
+    public byte[] dispersa(String cadena) {
+        byte[] bytesCadena = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance(TIPO_DE_DISPERSION);
+            bytesCadena = digest.digest(cadena.getBytes(StandardCharsets.UTF_8));
+            return bytesCadena;
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("El algoritmo que se trata de usar no existe");
+        }
+        return bytesCadena;
+    }
+}
