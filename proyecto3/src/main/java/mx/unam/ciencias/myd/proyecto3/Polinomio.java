1e8069475bb328b7050c73e5f72f0d928872c763
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2023-12-09 17:41:58 -0600

    Interpolacion para biginteger parece funcionar

--- proyecto3/src/main/java/mx/unam/ciencias/myd/proyecto3/Polinomio.java
+++ proyecto3/src/main/java/mx/unam/ciencias/myd/proyecto3/Polinomio.java
@@ -2,21 +2,27 @@ package mx.unam.ciencias.myd.proyecto3;
 
 import mx.unam.ciencias.myd.proyecto3.ParOrdenado;
 
+import java.math.BigInteger;
+import java.math.MathContext;
+
+
 /**
  * <p> Clase para representar polinomios.</p>
  * 
- * <p> La clase cuenta con métodos para realizar operaciones de polinomios.</p>
+ * <p> La clase cuenta con métodos para realizar operaciones de polinomios con enteros.</p>
  
  */
 
 public class Polinomio{
 
+    public static final BigInteger MODP = new BigInteger("208351617316091241234326746312124448251235562226470491514186331217050270460481");
+
     /** El grado del polinomio. */
-    int grado;
+    private int grado;
     /** Los coeficientes del polinomio. El indice i se 
      * encuentra el coeficiente termino con grado i.
     */
-    int[] coeficientes;
+    private int[] coeficientes;
 
     /** 
      * Constructor que recibe un arreglo de coeficientes.
@@ -49,7 +55,7 @@ public class Polinomio{
      * @param grado el grado del polinomio.
      * @return el coeficiente del polinomio.
      */
-    public long getCoeficiente(long grado){
+    public int getCoeficiente(int grado){
         return coeficientes[grado];
     }
 
@@ -85,7 +91,7 @@ public class Polinomio{
      */
     public static long interpolacion(ParOrdenado<Integer>[] puntos){
         int n = puntos.length;
-        long k=0;
+        double k=0;
         double num= 1;
         double den = 1;
         for(int i = 0; i < n; i++){
@@ -96,13 +102,43 @@ public class Polinomio{
                 num *= puntos[j].getX();
                 den *= puntos[i].getX() - puntos[j].getX();
             }
-            k += (int)num/den;
+            k += num/den;
             num = 1;
             den = 1;
         }
 
         if(n % 2 == 0)
             k = -k;
+        return (long)k;
+    }
+
+
+    /**
+     * 
+     * Realiza una version simplificada de la interpolacion
+     * de polinomios de lagrange para obtener el termino 
+     * independiente de un polinomio dado n puntos.
+     */
+    public static BigInteger interpolacionBig(ParOrdenado<BigInteger>[] puntos){
+        int n = puntos.length;
+        BigInteger k= new BigInteger("0");
+        BigInteger num= new BigInteger("1");
+        BigInteger den = new BigInteger("1");
+        for(int i = 0; i < n; i++){
+            num = puntos[i].getY().mod(MODP);
+            for(int j = 0; j < n; j++){
+                if(i == j)
+                    continue;
+                num = num.multiply(puntos[j].getX()).mod(MODP);
+                den = den.multiply(puntos[i].getX().subtract(puntos[j].getX())).mod(MODP);
+            }
+            BigInteger div = num.multiply(den.modInverse(MODP)).mod(MODP);
+            k = k.add(div).mod(MODP);
+            num = new BigInteger("1");
+            den = new BigInteger("1");
+        }
+        if(n % 2 == 0)
+            k = k.negate();
         return k;
     }
 }
--- proyecto3/src/main/java/mx/unam/ciencias/myd/proyecto3/Proyecto3.java
+++ proyecto3/src/main/java/mx/unam/ciencias/myd/proyecto3/Proyecto3.java
@@ -2,6 +2,8 @@ package mx.unam.ciencias.myd.proyecto3;
 
 import mx.unam.ciencias.myd.proyecto3.ParOrdenado;
 import mx.unam.ciencias.myd.proyecto3.Polinomio;
+import java.math.BigInteger;
+
 
 /**
  * Proyecto 3: Secreto compartido Shamir.
@@ -24,12 +26,24 @@ public class Proyecto3 {
 
     /* Instanciamos una AplicacionShamir y la ejecutamos.*/
     public static void main(String[] args) {
-        ParOrdenado<Integer>[] puntos = new ParOrdenado[4];
+        ParOrdenado<Integer>[] puntos = new ParOrdenado[6];
 
-        puntos[0] = new ParOrdenado(-1,24);
-        puntos[1] = new ParOrdenado(1,-4);
-        puntos[2] = new ParOrdenado(2,-27);
-        puntos[3] = new ParOrdenado(3,-88);
+        puntos[0] = new ParOrdenado(-3,-1968);
+        puntos[1] = new ParOrdenado(-2,-360);
+        puntos[2] = new ParOrdenado(-1,-52);
+        puntos[3] = new ParOrdenado(1,-24);
+        puntos[4] = new ParOrdenado(2,92);
+        puntos[5] = new ParOrdenado(3,900);
         System.out.println(Polinomio.interpolacion(puntos));
+        //Vamos a llenar con los mismos valores de arriba pero con BigInteger
+        ParOrdenado<BigInteger>[] puntosBig = new ParOrdenado[6];
+
+        puntosBig[0] = new ParOrdenado(new BigInteger("-3"),new BigInteger("-1968"));
+        puntosBig[1] = new ParOrdenado(new BigInteger("-2"),new BigInteger("-360"));
+        puntosBig[2] = new ParOrdenado(new BigInteger("-1"),new BigInteger("-52"));
+        puntosBig[3] = new ParOrdenado(new BigInteger("1"),new BigInteger("-24"));
+        puntosBig[4] = new ParOrdenado(new BigInteger("2"),new BigInteger("92"));
+        puntosBig[5] = new ParOrdenado(new BigInteger("3"),new BigInteger("900"));
+        System.out.println(Polinomio.interpolacionBig(puntosBig));
     }
 }
